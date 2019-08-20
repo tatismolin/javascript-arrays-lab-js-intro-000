877e2283993edf7f3267e7d780e49e734e5a4c70
immutable-illustrator-3998 <tatismolin@gmail.com>
2019-08-20 23:07:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function appendKitten(name){
 }
 
 function prependKitten(name){ //need fix
-  var a = kittens.unshift('Arnold');
-  return a;
+  return kittens.unshift('Arnold');
+  return kittens;
 }
 
 function removeLastKitten(){
